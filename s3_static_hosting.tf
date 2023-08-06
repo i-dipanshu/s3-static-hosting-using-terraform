b3d9d646de70427b8c74eb16d67f20f6c2e2eca2
Dipanshu <medipansh@gmail.com>
2023-08-07 00:58:20 +0530

    removed redundant bucket policy

--- s3_static_hosting.tf
+++ s3_static_hosting.tf
@@ -13,10 +13,6 @@ resource "aws_s3_bucket_website_configuration" "s3_bucket_config" {
   }
 }
 
-resource "aws_s3_bucket_policy" "s3_bucket_policy" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  policy = templatefile("s3_bucket_policy.json", { bucket = var.BUCKET_NAME })
-}
 
 resource "aws_s3_bucket_public_access_block" "mybucket" {
   bucket = aws_s3_bucket.s3_bucket.id
