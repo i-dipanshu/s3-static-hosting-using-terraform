18dba46503a632503f765bde85350bcd7430d24c
Dipanshu <medipansh@gmail.com>
2023-08-07 01:04:12 +0530

    edit index to trigger CI/CD and cache invalidation

--- s3_static_hosting.tf
+++ s3_static_hosting.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.BUCKET_NAME
 }
 
-
 // Enabling static hosting to the above created bucket
 resource "aws_s3_bucket_website_configuration" "s3_bucket_config" {
   bucket = aws_s3_bucket.s3_bucket.id
@@ -13,12 +12,8 @@ resource "aws_s3_bucket_website_configuration" "s3_bucket_config" {
   }
 }
 
-
 resource "aws_s3_bucket_public_access_block" "mybucket" {
   bucket = aws_s3_bucket.s3_bucket.id
-
   block_public_acls       = true
   block_public_policy     = true
-  //ignore_public_acls      = true
-  //restrict_public_buckets = true
 }
